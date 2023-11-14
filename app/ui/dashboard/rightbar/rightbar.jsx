a5859055bb2d46998638c77f9fb3f372c758443c
Edwin Kiyimba <edwinkiyimba@gmail.com>
2023-11-15 00:22:23 +0300

    Added chart, transactions and card components to dashboard

---
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -0,0 +1,6 @@
+import styles from "./rightbar.module.css";
+
+const RightBar = () => {
+	return <div>RightBar</div>;
+};
+export default RightBar;
---
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -0,0 +1,6 @@
+import styles from "./transactions.module.css";
+
+const Transactions = () => {
+	return <div>Transactions</div>;
+};
+export default Transactions;
