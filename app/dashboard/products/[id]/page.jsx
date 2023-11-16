2d0abbdcb3d929d593678aa62c9ab8c6e8482992
Edwin Kiyimba <edwinkiyimba@gmail.com>
2023-11-16 14:34:35 +0300

    Adding mongoose db

--- app/dashboard/products/[id]/page.jsx
+++ app/dashboard/products/[id]/page.jsx
@@ -1,6 +1,50 @@
+import styles from "@/app/ui/dashboard/products/singlePropduct/singleProduct.module.css";
+import Image from "next/image";
+
 const SingleProductPage = () => {
-  return (
-    <div>page</div>
-  )
-}
+	return (
+		<div className={styles.container}>
+			<div className={styles.infoContainer}>
+				<div className={styles.imgContainer}>
+					<Image src="/noavatar.png" alt="" fill />
+				</div>
+				Iphone
+			</div>
+			<div className={styles.formContainer}>
+				<form action="" className={styles.form}>
+					<label>Title</label>
+					<input type="text" name="title" placeholder="Iphone" />
+
+					<label>Price</label>
+					<input type="number" name="price" placeholder="$1129" />
+
+					<label>Stock</label>
+					<input type="number" name="stock" placeholder="70" />
+
+					<label>Color</label>
+					<input type="text" name="color" placeholder="red" />
+
+					<label>Size</label>
+					<input type="text" name="size" placeholder="Plus" />
+
+					<label>Category</label>
+					<select name="cat" id="cat">
+						<option value="phone">Phone</option>
+						<option value="kitchen">Kitchen</option>
+						<option value="computers">Computers</option>
+					</select>
+
+					<label>Description</label>
+					<textarea
+						name="desc"
+						id="desc"
+						rows="16"
+						placeholder="Description"></textarea>
+
+					<button>Update</button>
+				</form>
+			</div>
+		</div>
+	);
+};
 export default SingleProductPage;
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,15 @@
-export const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import styles from "@/app/ui/login/login.module.css";
+
+const LoginPage = () => {
+	return (
+		<div className={styles.container}>
+			<form action="" className={styles.form}>
+				<h1>Login</h1>
+				<input type="text" placeholder="username" />
+				<input type="text" placeholder="password" />
+				<button>Login</button>
+			</form>
+		</div>
+	);
+};
+export default LoginPage;
